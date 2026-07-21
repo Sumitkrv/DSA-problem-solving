85778a7e691e4809a0e8c457c4d0351247a10b0c
SUMIT <143883046+Sumitkrv@users.noreply.github.com>
2026-07-22 01:08:28 +0530

    Time: 0 ms (100.00%) | Memory: 60.9 MB (64.25%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
+        int n=nums.size();
+        int ans=1;
+        for(int i=0; i<n; i++){
+            int mask=0;
+            for(int j=i; j<n; j++){
+                if(mask &nums[j] ) break;
 
-        int left = 0;
-        int mask = 0;
-        int ans = 0;
-
-        for (int right = 0; right < nums.size(); right++) {
-
-            while (mask & nums[right]) {
-                mask ^= nums[left];
-                left++;
+                mask|=nums[j];
+                ans=max(ans, j-i+1);
             }
-
-            mask |= nums[right];
-
-            ans = max(ans, right - left + 1);
         }
-
         return ans;
     }
 };
