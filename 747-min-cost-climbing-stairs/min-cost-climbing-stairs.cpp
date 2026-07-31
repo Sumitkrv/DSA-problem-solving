8147e95ba28e4ecb2786e4d15b73f56745ef510b
SUMIT <143883046+Sumitkrv@users.noreply.github.com>
2026-07-31 14:38:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (52.84%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    int f(int ind, vector<int>& cost, vector<int> &dp){
-        if(ind==0 || ind==1){
-            return cost[ind];
-        }
-        if(dp[ind]!=-1) return dp[ind];
-        int mini=INT_MAX;
-        int one_step=cost[ind]+ f(ind-1, cost, dp);
-        int two_step=cost[ind]+ f(ind-2, cost, dp);
-        mini=min(mini, min(one_step,two_step));
-        return dp[ind]= mini;
+    // int f(int ind, vector<int>& cost, vector<int> &dp){
+    //     if(ind==0 || ind==1){
+    //         return cost[ind];
+    //     }
+    //     if(dp[ind]!=-1) return dp[ind];
+    //     int mini=INT_MAX;
+    //     int one_step=cost[ind]+ f(ind-1, cost, dp);
+    //     int two_step=cost[ind]+ f(ind-2, cost, dp);
+    //     mini=min(mini, min(one_step,two_step));
+    //     return dp[ind]= mini;
 
-    }
+    // }
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        vector<int> dp(n, -1);
-        return min(f(n-1, cost, dp), f(n-2, cost, dp));
+        vector<int> dp(n+1, 0);
+        for(int ind=0; ind<n; ind++){
+            if(ind==1|| ind==0){
+                dp[ind]=cost[ind];
+                continue;
+            }
+            
+            int mini=INT_MAX;
+            int one_step=cost[ind]+ dp[ind-1];
+            int two_step=cost[ind]+ dp[ind-2];
+            mini=min(mini, min(one_step,two_step));
+            dp[ind]= mini;
+        }
+
+        return min(dp[n-1],dp[n-2]);
     }
 };
