1380fdf34bec99f3b099d49545c1702ef4e52dfd
SUMIT <143883046+Sumitkrv@users.noreply.github.com>
2026-07-31 16:27:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (24.40%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    int f(int ind, vector<int>& nums, vector<int>& dp){
-        if(ind==0){
-            return nums[ind];
-        }
-        if(ind<0){
-            return 0;
-        }
-        if(dp[ind]!=-1) return dp[ind];
-        int notTake=0+f(ind-1, nums, dp);
-        int take=0;
-        if(ind>=1){
-             take=nums[ind]+f(ind-2, nums, dp);
-        }
-        return dp[ind]= max(take, notTake);
-    }
+    // int f(int ind, vector<int>& nums, vector<int>& dp) {
+    //     if (ind == 0) {
+    //         return nums[ind];
+    //     }
+    //     if (ind < 0) {
+    //         return 0;
+    //     }
+    //     if (dp[ind] != -1)
+    //         return dp[ind];
+    //     int notTake = 0 + f(ind - 1, nums, dp);
+    //     int take = 0;
+    //     if (ind >= 1) {
+    //         take = nums[ind] + f(ind - 2, nums, dp);
+    //     }
+    //     return dp[ind] = max(take, notTake);
+    // }
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> dp(n, -1);
-        return f(n-1, nums, dp);
+        int n = nums.size();
+        vector<int> dp(n+1, 0);
+        // dp[0]=0;
+         dp[1] = nums[0];
+        for (int ind = 1; ind < n; ind++) {
+            int notTake = 0 + dp[ind];
+            int take = 0;
+            if (ind >= 1) {
+                take = nums[ind] + dp[ind - 2 + 1];
+            }
+            dp[ind + 1] = max(take, notTake);
+        }
+        return dp[n];
     }
 };
