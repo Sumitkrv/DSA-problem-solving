49ff90f197463a0cc125f7d3d6a7d9df8a409e3b
SUMIT <143883046+Sumitkrv@users.noreply.github.com>
2026-08-03 01:34:22 +0530

    Time: 44 ms (74.74%) | Memory: 29.2 MB (47.38%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -7,25 +7,23 @@ public:
         for (int i = 0; i < n - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
-
             int left = i + 1;
             int right = n - 1;
             while (left < right) {
-                int sum = nums[left] + nums[i] + nums[right];
+                int sum = nums[right] + nums[i] + nums[left];
                 if (sum == 0) {
-                    ans.push_back({nums[i], nums[left], nums[right]});
+                    ans.push_back({nums[i], nums[right], nums[left]});
                     left++;
                     right--;
                     while (left < right && nums[left] == nums[left - 1]) {
                         left++;
                     }
-                    while (left < right && nums[right] == nums[right+1]) {
+                    while (left < right && nums[right] == nums[right + 1]) {
                         right--;
                     }
                 } else if (sum < 0) {
                     left++;
-                } 
-                else {
+                } else {
                     right--;
                 }
             }
