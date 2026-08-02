dd9ec4f1a1fe1a7af5eaa86457c713eb43312dcb
SUMIT <143883046+Sumitkrv@users.noreply.github.com>
2026-08-03 00:44:02 +0530

    Time: 15 ms (20.49%) | Memory: 19.6 MB (20.08%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
+    int f(int i, int j, vector<int>& piles, vector<vector<int>> &dp){
+        if(i==j) return piles[i];
+        if(dp[i][j]!=-1) return dp[i][j];
+        int left=piles[i]-f(i+1, j, piles, dp);
+        int right=piles[j]-f(i, j-1, piles, dp);
+        int maxi=max(left, right);
+        return dp[i][j]= maxi;
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n=piles.size();
+        vector<vector<int>> dp(n, vector<int>(n,-1));
+        return f(0, n-1, piles, dp)>0;
     }
 };
