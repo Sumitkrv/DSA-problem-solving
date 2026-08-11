30db9ba8a62163a4afd8c7ccfc50653e1a7f1a18
SUMIT <143883046+Sumitkrv@users.noreply.github.com>
2026-08-11 11:05:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (65.84%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -9,13 +9,15 @@ public:
     // }
     int fib(int n) {
         if(n<=1) return n;
-        vector<int> dp(n+1, 0);
-        dp[0]=0;
-        dp[1]=1;
+        //vector<int> dp(n+1, 0);
+        int prev1=1, prev2=0;
+        
         for(int i=2; i<=n; i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
         }
 
-        return dp[n];
+        return prev1;
     }
 };
