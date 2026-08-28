07320e2448bb8465f51e7d22b47d1e53a7bb25dd
SUMIT <143883046+Sumitkrv@users.noreply.github.com>
2026-08-29 01:32:20 +0530

    Time: 3 ms (9.18%) | Memory: 10.2 MB (6.36%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n=s.size();
+        //if(n==1) return false;
+        //if(s[0]==')' || s[0]==']' || s[0]=='}') return false;
+        stack<int> st;
+        for(char ch: s){
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()) return false;
+                if(st.top()=='(' && ch==')'){
+                    st.pop();
+                }
+                else if(st.top()=='[' && ch==']'){
+                    st.pop();
+                }
+                else if(st.top()=='{' && ch=='}'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(st.empty()) return true;
+        return false;
+
+    }
+};
